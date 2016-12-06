d4ede71022dd6f22b0d9382bcbab7902b2e85ba9
Taylor King <dekkofilms@gmail.com>
2016-12-06 12:52:28 -0600

    added facebook auth

---
+++ socialnetworktutorial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  socialnetworktutorial
+//
+//  Created by Taylor King on 12/6/16.
+//  Copyright © 2016 Taylor King. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ socialnetworktutorial/RoundButton.swift
@@ -0,0 +1,28 @@
+//
+//  RoundButton.swift
+//  socialnetworktutorial
+//
+//  Created by Taylor King on 12/6/16.
+//  Copyright © 2016 Taylor King. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = 5.0
+    }
+
+}
